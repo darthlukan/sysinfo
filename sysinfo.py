d2256be3c8dd4a233c7d66f6b7d00f8de0019703
Brian Tomlinson <darthlukan@gmail.com>
2015-08-30 16:07:03 +0200

    Add initial arguments and help messages

--- sysinfo.py
+++ sysinfo.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import os
 import psutil
+import argparse
 from datetime import datetime
 
 
@@ -55,6 +56,21 @@ def date_time():
 
 
 def main():
+    parser = argparse.ArgumentParser(
+        description='Available argument information.')
+    parser.add_argument('-d', '--disks', nargs='*', type=str, action='store',
+                        dest='disks', help='The disks to lookup.')
+    parser.add_argument('-c', '--cpu', type=bool, action='store', dest=cpu,
+                        help='Whether or not to show CPU usage.')
+    parser.add_argument('-m', '--mem', type=bool, action='store', dest=mem,
+                        help='Whether or not to show Memory usage.')
+    parser.add_argument('-l', '--load', type=bool, action='store', dest=load,
+                        help='Whether or not to show Load averages.')
+    parser.add_argument('-dt', '--date-time', type=str, action='store',
+                        dest=date_time, default='%Y-%m-%d %H:%M:%S',
+                        help='Show the datetime using the following format.')
+    args = parser.parse_args()
+
     return "{0} | {1} | {2} | {3} | {4}".format(
         disks(), cpu(), mem(), load(), date_time())
 
